96ce8b5e105019fdac4eb1eddd3c6b57cc223a24
Lucas Berg <berg@ice.ufjf.br>
2017-08-18 09:12:55 -0300

    Add input

---
+++ Two-n-Body/Parallel-Basic/src/main.c
@@ -0,0 +1,31 @@
+/*
+------------------------------------------------------------------------------------------------
+    Basic Parallel Version of the Two-N-Body algorithm
+        Autor: Lucas Berg
+
+- It is possible to build an input with executing the Python script (makeInput.py)
+
+- After an input has been made, you can execute the program by passing the generated file as:
+
+    $ ./twoBody < name_input_file
+------------------------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/twoBody.h"
+#include "../include/timer.h"
+
+int main (int argc, char *argv[])
+{
+    double start, finish, elapsed;
+    TwoBody *model = newTwoBody(argc,argv);
+
+    GET_TIME(start);
+    solveModel(model);
+    GET_TIME(finish);
+    elapsed = finish - start;
+    printf("Elapsed time = %.10lf\n",elapsed);
+
+    return 0;
+}
